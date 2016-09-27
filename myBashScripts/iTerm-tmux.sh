2542d6c65e62a81d0f7eb0b5a76eba04ce82d87c
aReigoto <andrereigoto@gmai.com>
2016-09-27 16:26:35 +0100

    vim

--- myBashScripts/iTerm-tmux.sh
+++ myBashScripts/iTerm-tmux.sh
@@ -1,7 +1,9 @@
-if [[ $(tmux ls | wc -l | xargs) == "0" ]] ; then
-	tmux new -s $(hostname -s) ;
-elif [[ $(tmux ls | wc -l | xargs) == "1" ]] ; then
-	tmux a ;
-else echo -e "tmux a -t .......\n" ;
-	tmux ls ;
-fi
+echo "experiments runing"
+echo "tmux a -t ......"
+# if [[ $(tmux ls | wc -l | xargs) == "0" ]] ; then
+#     tmux new -s $(hostname -s) ;
+# elif [[ $(tmux ls | wc -l | xargs) == "1" ]] ; then
+#     tmux a ;
+# else echo -e "tmux a -t .......\n" ;
+#     tmux ls ;
+# fi
