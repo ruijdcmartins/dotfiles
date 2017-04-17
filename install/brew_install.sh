afc3ed77a679f93921337c9c6920a4a0793f6838
aReigoto <andre.reigoto@gmail.com>
2017-04-17 15:47:48 +0100

    vim stuff

--- install/brew_install.sh
+++ install/brew_install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo -e "Aborting the install process: This script must be run as root\n"
-   exit;
+  echo -e "Aborting the install process: This script must be run as root\n"
+  return 1 2>/dev/null || exit 
 fi
 
 logFolderBrew="$HOME/logs/dotfilesLogs/brewLogs/brew/"
 
 if [[ ! -d $logFolderBrew ]]; then
-    mkdir -p $logFolderBrew
+  mkdir -p $logFolderBrew
 fi
 
 
 if [[ $OSTYPE =~ "darwin" ]]; then
-    if ! ( xcode-select -v ) ; then
+  if ! ( xcode-select -v ) ; then
     xcode-select --install ||  { echo -e "<===========> \nErro installing xcode!" ; return 1 ; }
-    #return 0
-    fi
+  #return 0
+  fi
 
-    if ! ( brew -v &> /dev/null ) ; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" | \
-        tee ${logFolderBrew}brewInstall.log || { echo -e "<===========> \nErro brew install!" ; return 1 ; }
-        brew doctor | tee ${logFolderBrew}brewDoctor.log || { echo -e "<===========> \nErro brew doc!" ; return 1  ; }
-    #return 0
-    fi
+  if ! ( brew -v &> /dev/null ) ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" | \
+    tee ${logFolderBrew}brewInstall.log || { echo -e "<===========> \nErro brew install!" ; return 1 ; }
+    brew doctor | tee ${logFolderBrew}brewDoctor.log || { echo -e "<===========> \nErro brew doc!" ; return 1  ; }
+  #return 0
+  fi
 fi
