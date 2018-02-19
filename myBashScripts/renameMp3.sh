4e117f17911fe42e056527de3933609dd31b0501
aReigoto <andrereigoto@gmai.com>
2018-02-19 18:50:08 +0000

    "vimrc"

--- myBashScripts/renameMp3.sh
+++ myBashScripts/renameMp3.sh
@@ -8,28 +8,21 @@
 # perl -ne 'if (/([0-9]+)(?: - )(.*mp3)/) { print "$1 $2" } '
 # perl -ne 'if (/^([0-9-]+)?(?: - |-| -| -|\.|\. )?(.*mp3)$/) { print "$1 $2" } '
 
-regex='/^([0-9-]+)(?: - |-| -| -|\.|\. )(.*mp3)$/'
-#  perl -ne 'if ('"$regex"') { print "$1 $2" } '
-
-rename() {
-  # newName=` echo $1 | perl -pe "$regex" `
-  newName=` echo $1 | perl -ne 'if ('"$regex"') { print "$1 $2" } ' `
-  if [ ! -z "$2" ] && [ "$2" == '-v' ] ; then
-    echo -e "$file ->\t $newName"
-  elif [ ! -z "$newName" ] ; then
-    # echo "dry run $2"
-    mv "$1" "$newName"
-  fi
-}
-
+#---------- ----------#
 # declare defouts
-arrayOfFolders=()
-inVar="."
-# inVar="*/"
+#---------- ----------#
+defaultFolder="."
+# defaultFolder="*/"
 test="-v"
 fileExtention="*.mp3"
+# fileExtention="*.mp3*" # get files with space at the end
+regex='/^([0-9-]+)(?: - |-| -| -|\.|\. )(.*\.mp3)$/'
+
+# regex='/^([0-9-]+)(.*)(\.mp3\s+)$/'
+#  perl -ne 'if ('"$regex"') { print "$1 $2" } '
 
 
+#---------- ----------#
 while [[ $1 =~ ^- ]] ; do
   case $1 in
     -e | --execute)
@@ -37,7 +30,7 @@ while [[ $1 =~ ^- ]] ; do
       shift
        ;;
     -d | --directory)
-      inVar=$2
+      defaultFolder=$2
       shift
       shift
       ;;
@@ -49,13 +42,27 @@ while [[ $1 =~ ^- ]] ; do
   esac
 done
 
+arrayOfFolders=($defaultFolder)
 
+#---------- ----------#
+rename() {
+  # newName=` echo $1 | perl -pe "$regex" `
+  newName=` echo $1 | perl -ne 'if ('"$regex"') { print "$1$2.mp3" } ' `
+  if [ ! -z "$2" ] && [ "$2" == '-v' ] ; then
+    echo -e "$file ->\t $newName"
+  elif [ ! -z "$newName" ] ; then
+    # echo "dry run $2"
+    mv "$1" "$newName"
+  fi
+}
+
+
+#---------- ----------#
 # Store folders in a array and print the total number of elements
 # for folder in */ ; do arrayOfFolders+=("$folder") ; done ; echo ${#arrayOfFolders[@]}
 # Check the array content
 # for folder in "${arrayOfFolders[@]}" ; do echo "$folder" ; done
 
-arrayOfFolders=($inVar)
 
 for folder in "${arrayOfFolders[@]}" ; do
     if [[ -d "$folder" ]]; then
