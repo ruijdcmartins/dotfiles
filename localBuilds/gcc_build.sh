d3cf8a25ef716b4df641f880c0f7829e73bbc345
aReigoto <andre.reigoto@gmail.com>
2018-03-28 02:12:04 +0100

    "local build gcc"

---
+++ localBuilds/gcc_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gcc_version=5.2.0
+# gcc_version=7.3.0
+build_foler=~/TEMP/gcc_${gcc_version}
+mkdir -p $build_foler
+cd $build_foler
+
+wget http://www.netgull.com/gcc/releases/gcc-${gcc_version}/gcc-${gcc_version}.tar.gz
+
+mkdir -p $HOME/.local/gcc-${gcc_version}
+
+tar xzf gcc-${gcc_version}.tar.gz
+cd gcc-${gcc_version}
+./contrib/download_prerequisites
+cd ..
+mkdir objdir
+cd objdir
+$PWD/../gcc-${gcc_version}/configure --prefix=$HOME/.local/gcc-${gcc_version} --enable-languages=c,c++,fortran,go
+make
+make install
+
+cat<<EOF >> ~/.bash_profile
+#=============== GCC =================
+gcc_version=${gcc_version}
+export PATH=~/.local/gcc-\${gcc_version}/bin:\$PATH
+export LD_LIBRARY_PATH=~/.local/gcc-\${gcc_version}/lib:\$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=~/.local/gcc-\${gcc_version}/lib64:\$LD_LIBRARY_PATH
+export CC=~/.local/gcc-\${gcc_version}/bin/gcc
+export CXX=~/.local/gcc-\${gcc_version}/bin/g++
+export FC=~/.local/gcc-\${gcc_version}/bin/gfortran
+#======================================
+EOF
+
