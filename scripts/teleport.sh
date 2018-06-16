936a0180e142fb9538493244193337671ffad15a
aReigoto <andre.reigoto@gmail.com>
2018-06-16 23:09:24 +0100

    teleport sh

---
+++ scripts/teleport.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+location_of_dic=~/.dotfiles/localFiles/teleport_folders.sh
+
+if [[ -e $location_of_dic ]]; then
+  source $location_of_dic
+else 
+  declare -A dic_folders
+fi
+
+add_destination(){
+if [[ -z $1  ]] ; then
+  folder=$PWD
+else
+  folder=$1
+fi
+echo -e "Name this teleportation folder:\n $folder"
+read -ep "$(echo $'\n> ')" folder_alias
+dic_folders+=(["$folder_alias"]="$folder")
+}
+
+print_dic(){
+echo ""
+counter=1
+for key in "${!dic_folders[@]}"; do 
+  echo "$counter => $key - ${dic_folders[$key]}";
+  ((counter++))
+done
+}
+
+save_to_file(){
+echo "declare -A dic_folders=(" > $location_of_dic
+for key in "${!dic_folders[@]}"; do 
+  echo "[\"$key\"]=\"${dic_folders[$key]}\"" >> $location_of_dic;
+done
+echo ")" >> $location_of_dic
+}
+
+folder_by_index(){
+if [[ $1 -le "${#dic_folders[@]}" ]]; then
+  counter=1
+  for key in "${!dic_folders[@]}"; do 
+    if [[ $1 -ne $counter ]]; then
+      ((counter++))
+      continue
+    else
+      cd ${dic_folders[$key]}
+      break
+    fi
+  done
+fi
+}
+
+folder_by_key(){
+for key in "${!dic_folders[@]}"; do 
+  if [[ $key =~ $1 ]]; then
+    cd ${dic_folders[$key]}
+    break
+  fi
+done
+}
+
+#define options via flagged input arguments
+check_args(){
+if [[ $# -eq 0 ]] ; then
+  print_dic
+  read -ep "\nHere to go?$(echo $'\n> ')" choice
+elif [[ $# -eq 1 ]] ; then
+  choice=$1
+elif [[ $# -eq 2 ]] && [[ $1 =~ -a ]] ; then
+    add_destination $2
+    save_to_file
+fi
+
+case $choice in
+  [0-9] | [0-9][0-9])
+    folder_by_index $choice
+    ;;
+  -a | -add)
+    add_destination
+    save_to_file
+    ;;
+  *)
+    folder_by_key $choice
+    ;;
+
+esac
+}
+
+check_args $@
+
